3e80526dc9a495b4c62bf38ffae14564c12a6b67
Randy <randy_maclean@hotmail.ca>
2021-05-01 20:30:26 -0700

    Added functionality to 3rd set of lyrics

--- main.js
+++ main.js
@@ -56,10 +56,20 @@ nextOne.onclick = function() {
 nextTwo.onclick = function() {
     nextThree.hidden = false;
     nextTwo.hidden = true;
+    document.getElementById('word-five').innerHTML = 'DEAR';
+    document.getElementById('word-six').innerHTML = 'FRI-';
+    lastLyric.style.display = 'inline-block';
+    document.getElementById('letter-note-three').innerHTML = 'G';
+    document.getElementById('letter-note-four').innerHTML = 'E';
+    document.getElementById('letter-note-five').innerHTML = 'C';
+    document.getElementById('letter-note-six').innerHTML = 'B';
 };
 
 // Write anonymous event handler property and function for the third progress button
-
+nextThree.onclick = function() {
+    startOver.hidden = false;
+    nextThree.hidden = true;  
+}
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
